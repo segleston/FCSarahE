7fde08744975d253f5b887e5ec5009c30e0ad77e
segleston <segleston@gmail.com>
2024-04-15 21:03:18 +0100

    added js for projects

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,47 @@
-// Select the arrow element
 const arrow = document.getElementById('arrow');
 
-// Add click event listener to the arrow
+// scroll to top on click
 arrow.addEventListener('click', function() {
-    // Scroll to the top of the page smoothly
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 });
+
+const fadeInAnimationVariants = {
+    initial: {
+        opacity: 0,
+        y: 100,
+    },
+    animate: (index) => ({
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: 0.1 * index,
+        }
+    })
+};
+
+//  Fade in animation for project elements on the home
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          const targetElement = entry.target;
+          let animationSettings = fadeInAnimationVariants.animate(index);
+          targetElement.style.opacity = animationSettings.opacity;
+          targetElement.style.transform = `translateY(${animationSettings.y}px)`;
+          targetElement.style.transitionDelay = `${animationSettings.transition.delay}s`;
+  
+          observer.unobserve(targetElement);
+        }
+      });
+    }, {
+      rootMargin: '0px',
+      threshold: 0.5
+    });
+  
+    document.querySelectorAll('.fade-in-section').forEach((section, index) => {
+      observer.observe(section);
+    });
+  });
